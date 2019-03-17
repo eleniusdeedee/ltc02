5352799fc3c13053b7c4d6d674c428d74f302a9b
Elena Doperchuk <elenius@jays.local>
2019-03-17 17:59:13 +0000

    hello.c

---
+++ c/hello.c
@@ -0,0 +1,20 @@
+/*	Description	: Print Hello World on the screen
+
+		Author		: Elena Doperchuk
+
+		Date		: /01/2007				*/
+
+	#include<stdio.h>
+
+
+	int main()
+
+	{
+
+		//Print something and then newline
+
+		printf(“Hello World\n”);
+
+		return 0;
+
+	}
