4cea72ad2b79f1ea9f112824e4acba56942ea776
elena <elena4grad@gmail.com>
2019-03-25 15:26:21 +0000

    another verse as a variable - poem5

--- js/99bottles.js
+++ js/99bottles.js
@@ -2,26 +2,27 @@ function beerpoem (count)
 {
     
     let poem1 = " bottles of beer on the wall, ";
-    let poem2 = " bottles of beer.\n\ Take one down, pass it around, "
-    let poem3 = " bottles of beer on the wall...\n\ "
+    let poem2 = " bottles of beer.\n"
+    let poem3 = " bottles of beer on the wall...\n "
     let poem4 = "No more bottles of beer on the wall, no more bottles of beer. \n\
                 We've taken them down and passed them around; now we're drunk and passed out!"
+    let poem5 = "Take one down, pass it around, ";
     if (count == 1)
     {
-		console.log("1 bottle of beer on the wall, 1 bottle of beer.\n\ " + count);
-		console.log("Take one down, pass it around, no more bottles of beer on the wall...\n\ ");
+		console.log("1 bottle of beer on the wall, 1 bottle of beer.\n" + count);
+		console.log(poem5 + " no more bottles of beer on the wall...\n\ ");
         console.log (poem4);
     }
     else if (count == 2)
     {
         console.log(count + poem1 + count + poem2 + (count-1) + "bottle of beer on the wall...\n\  ");
-		console.log("Take one down, pass it around, 1 bottle of beer on the wall...\n\ ");
+		console.log(poem5 + "1 bottle of beer on the wall...\n\ ");
     }
     else
     {	
-    console.log(count + poem1 + count + poem2 + (count-1) + poem3); 
-    count = count - 1;
-    beerpoem(count);
+    console.log(count + poem1 + count + poem2 + poem5 + (count-1) + poem3); 
+    
+    beerpoem(count - 1);
     };
 }
 
