47cc8ee23e28fec4401474bcc993406dba15c3e4
elena <elena4grad@gmail.com>
2019-03-25 15:12:51 +0000

    count as a paramter

--- js/99bottles.js
+++ js/99bottles.js
@@ -1,13 +1,11 @@
-let poem1 = " bottles of beer on the wall, ";
-let poem2 = " bottles of beer.\n\ Take one down, pass it around, "
-let poem3 = " bottles of beer on the wall...\n\ "
-let poem4 = "No more bottles of beer on the wall, no more bottles of beer. \n\
-We've taken them down and passed them around; now we're drunk and passed out!"
-
-let count = 99
-
-function beerpoem () 
+function beerpoem (count) 
 {
+    
+    let poem1 = " bottles of beer on the wall, ";
+    let poem2 = " bottles of beer.\n\ Take one down, pass it around, "
+    let poem3 = " bottles of beer on the wall...\n\ "
+    let poem4 = "No more bottles of beer on the wall, no more bottles of beer. \n\
+                We've taken them down and passed them around; now we're drunk and passed out!"
     if (count == 1)
     {
 		console.log("1 bottle of beer on the wall, 1 bottle of beer.\n\ " + count);
@@ -23,9 +21,9 @@ function beerpoem ()
     {	
     console.log(count + poem1 + count + poem2 + (count-1) + poem3); 
     count = count - 1;
-    beerpoem();
+    beerpoem(count);
     };
 }
 
 
-beerpoem();
+beerpoem(99);
